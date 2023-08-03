78ae008e8ed63d039cec36b741751cc9d5266e28
JASWANTH <122432930+jaswanth2005@users.noreply.github.com>
2023-08-03 09:42:21 +0530

    Add files via upload

---
+++ Applicant.java
@@ -0,0 +1,79 @@
+
+
+/**
+ * Class Applicant
+ */
+public class Applicant {
+
+  //
+  // Fields
+  //
+
+  private new_class_2 applicant_name;
+  private new_class_3 applicant_details;
+  
+  //
+  // Constructors
+  //
+  public Applicant () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of applicant_name
+   * @param newVar the new value of applicant_name
+   */
+  private void setApplicant_name (new_class_2 newVar) {
+    applicant_name = newVar;
+  }
+
+  /**
+   * Get the value of applicant_name
+   * @return the value of applicant_name
+   */
+  private new_class_2 getApplicant_name () {
+    return applicant_name;
+  }
+
+  /**
+   * Set the value of applicant_details
+   * @param newVar the new value of applicant_details
+   */
+  private void setApplicant_details (new_class_3 newVar) {
+    applicant_details = newVar;
+  }
+
+  /**
+   * Get the value of applicant_details
+   * @return the value of applicant_details
+   */
+  private new_class_3 getApplicant_details () {
+    return applicant_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void apply_passport()
+  {
+  }
+
+
+  /**
+   */
+  public void submission_of_proof()
+  {
+  }
+
+
+}
